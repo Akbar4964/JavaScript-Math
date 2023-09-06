5985ecaf7e24b75cf74bc26c4a11abb6dba85bc5
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-06 10:52:01 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,44 @@
 // Math.round()
-console.log(Math.round(0.4));
-console.log(Math.round(0.5));
+// console.log(Math.round(0.4));
+// console.log(Math.round(0.5));
 
-console.log(Math.round(1.1));
-console.log(Math.round(1.7));
+// console.log(Math.round(1.1));
+// console.log(Math.round(1.7));
 
-console.log(Math.round(2.2));
-console.log(Math.round(2.5));
+// console.log(Math.round(2.2));
+// console.log(Math.round(2.5));
 
-console.log(Math.round(3.2));
-console.log(Math.round(3.9));
+// console.log(Math.round(3.2));
+// console.log(Math.round(3.9));
 
-console.log(Math.round(4.4));
-console.log(Math.round(4.8));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.8));
 
-console.log(Math.round(5.1));
-console.log(Math.round(5.5));
-console.log(Math.round(6.1));
-console.log(Math.round(6.6));
+// console.log(Math.round(5.1));
+// console.log(Math.round(5.5));
+// console.log(Math.round(6.1));
+// console.log(Math.round(6.6));
 
-console.log(Math.round(7.3));
-console.log(Math.round(7.7));
+// console.log(Math.round(7.3));
+// console.log(Math.round(7.7));
 
-console.log(Math.round(8.3));
-console.log(Math.round(8.8));
+// console.log(Math.round(8.3));
+// console.log(Math.round(8.8));
 
-console.log(Math.round(9.4));
-console.log(Math.round(9.9));
+// console.log(Math.round(9.4));
+// console.log(Math.round(9.9));
 
-console.log(Math.round(10.1));
-console.log(Math.round(10.5));
+// console.log(Math.round(10.1));
+// console.log(Math.round(10.5));
+
+// Math.ceil()
+console.log(Math.ceil(1));
+console.log(Math.ceil(1.1));
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(4.1));
+console.log(Math.ceil(5.1));
+console.log(Math.ceil(6.1));
+console.log(Math.ceil(7.1));
+console.log(Math.ceil(8.1));
+console.log(Math.ceil(9.1));
