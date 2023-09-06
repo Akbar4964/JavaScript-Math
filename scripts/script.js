270bf5798229ffc0c1ef362682d0b2846bcb3747
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-06 10:45:18 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
 // Math.round()
 console.log(Math.round(0.4));
 console.log(Math.round(0.5));
+
+console.log(Math.round(1.1));
+console.log(Math.round(1.7));
+
+console.log(Math.round(2.2));
+console.log(Math.round(2.5));
+
+console.log(Math.round(3.2));
+console.log(Math.round(3.9));
+
+console.log(Math.round(4.4));
+console.log(Math.round(4.8));
+
+console.log(Math.round(5.1));
+console.log(Math.round(5.5));
+console.log(Math.round(6.1));
+console.log(Math.round(6.6));
+
+console.log(Math.round(7.3));
+console.log(Math.round(7.7));
