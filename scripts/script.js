3afc9d4f44d34479190d85e52a39f390c5044262
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-06 10:54:27 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -32,13 +32,25 @@
 // console.log(Math.round(10.5));
 
 // Math.ceil()
-console.log(Math.ceil(1));
-console.log(Math.ceil(1.1));
-console.log(Math.ceil(2.1));
-console.log(Math.ceil(3.1));
-console.log(Math.ceil(4.1));
-console.log(Math.ceil(5.1));
-console.log(Math.ceil(6.1));
-console.log(Math.ceil(7.1));
-console.log(Math.ceil(8.1));
-console.log(Math.ceil(9.1));
+// console.log(Math.ceil(1));
+// console.log(Math.ceil(1.1));
+// console.log(Math.ceil(2.1));
+// console.log(Math.ceil(3.1));
+// console.log(Math.ceil(4.1));
+// console.log(Math.ceil(5.1));
+// console.log(Math.ceil(6.1));
+// console.log(Math.ceil(7.1));
+// console.log(Math.ceil(8.1));
+// console.log(Math.ceil(9.1));
+
+// Math.floor()
+console.log(Math.floor(1.9));
+console.log(Math.floor(2.9));
+console.log(Math.floor(3.9));
+console.log(Math.floor(4.9));
+console.log(Math.floor(5.9));
+console.log(Math.floor(6.9));
+console.log(Math.floor(7.9));
+console.log(Math.floor(8.9));
+console.log(Math.floor(9.9));
+console.log(Math.floor(10.9));
