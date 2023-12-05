f30d86afd067955d013abb6db1f3a07fcade5842
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-12-05 10:32:56 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -111,10 +111,8 @@
 // console.log(Math.min(11, 12, 13));
 // console.log(Math.min(14, 15, 16));
 // console.log(Math.min(17, 18, 19));
-
 // console.log(Math.min(20, 21, 22));
 // console.log(Math.min(23, 24, 25));
-
 // console.log(Math.min(26, 27, 28));
 
 // Math.max()
@@ -125,7 +123,6 @@
 // console.log(Math.max(11, 12, 13));
 // console.log(Math.max(14, 15, 16));
 // console.log(Math.max(17, 18, 19));
-
 // console.log(Math.max(20, 21, 22));
 // console.log(Math.max(23, 24, 25));
 // console.log(Math.max(26, 27, 28));
@@ -137,7 +134,6 @@
 // console.log(Math.trunc(Math.random() * 40));
 // console.log(Math.trunc(Math.random() * 50));
 // console.log(Math.trunc(Math.random() * 60));
-
 // console.log(Math.trunc(Math.random() * 70));
 // console.log(Math.trunc(Math.random() * 80));
 // console.log(Math.trunc(Math.random() * 90));
