d29266a9e78476dfb4684e5c2e9b2e3b52868108
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-12-10 21:46:53 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -139,3 +139,4 @@
 // console.log(Math.trunc(Math.random() * 90));
 // console.log(Math.trunc(Math.random() * 100));
 // console.log(Math.trunc(Math.random() * 100));
+// console.log(Math.trunc(Math.random() * 100));
